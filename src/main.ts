cdbb95e29ed92c8df158d6d502b8b03e20de4485
Vladislav <grishin1999@list.ru>
2023-10-08 00:03:42 +0300

    Фикс импорта axios

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import axios from "axios";
 
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
-app.use(axios)
 
 app.mount('#app')
